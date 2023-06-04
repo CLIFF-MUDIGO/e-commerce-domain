569c20922fd4d1835013969d115270b9b0637954
cliff <cliffmudigo5@gmail.com>
2023-06-04 19:03:59 +0300

    add instances

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,17 @@
-class Review <ActiveRecord::Base
-    
-end
+class Review < ActiveRecord::Base
+    belongs_to :product
+    belongs_to :user
+  
+    def reviewer
+      User.find(user_id)
+    end
+  
+    def reviewed_product
+      Product.find(product_id)
+    end
+  
+    def print_review
+      puts "Review for #{reviewed_product.name} by #{reviewer.name}: #{star_rating}. #{comment}"
+    end
+  end
+  
